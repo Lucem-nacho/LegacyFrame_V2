9146cdacde4f357ee5a0ac68d7115b3d28502e66
Nacho <i.gomezo@duocuc.cl>
2025-10-29 03:46:41 -0300

    fin?

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
-// 👇 CAMBIA 'vite' POR 'vitest/config'
 import { defineConfig } from 'vitest/config'; 
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  test: { // <-- El error rojo desaparecerá
+  test: { 
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
-    
-    // Dejamos esto listo para cuando ejecutes las pruebas
-    // y evitar el error del antivirus 
     environmentOptions: {
       jsdom: {
         resources: 'usable'
